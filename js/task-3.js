fc9c941a828406d2eb06c10274d48eefcfa28bf1
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-09-10 23:30:40 +0300

    Добавляет решения 2, 3 задач

---
+++ js/task-3.js
@@ -0,0 +1,62 @@
+// Write code under this line
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    const newItemList = [];
+    for (const product of this.items) {
+      if (product !== item) {
+        newItemList.push(product);
+      }
+    }
+
+    this.items = newItemList;
+
+    return this.items;
+  }
+}
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
